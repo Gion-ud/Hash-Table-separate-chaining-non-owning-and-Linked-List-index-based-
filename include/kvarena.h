a65c75853e77216205713986bde57098d2dd37e2
John_Wood <chiunyuwu@gmail.com>
2026-06-19 19:54:01 +1200

    added kvtbl.* that wraps up the hash table and arena with auto resize and manual gc; testingmake

--- include/kvarena.h
+++ include/kvarena.h
@@ -4,6 +4,7 @@
 #include <stdint.h>
 #include "kvht_key.h"
 #include "kvfile.h"
+#include "_kvapi.h"
 
 typedef KVFileEntry KVArenaEntry;
 
@@ -30,52 +31,52 @@ typedef struct KVArenaEntryView {
     uint32_t    val_len;
 } KVArenaEntryView;
 
-extern KVArena *create_kvarena(
+LIBKV_API KVArena *create_kvarena(
     uint32_t    bufsize,
     uint32_t    entrycap,
     uint32_t    align
 );
-extern void destroy_kvarena(KVArena *kva_p);
-extern int32_t kvarena_push(
+LIBKV_API void destroy_kvarena(KVArena *kva_p);
+LIBKV_API int32_t kvarena_push(
     KVArena            *kva_p,
     const kvht_key_t   *kvht_key_p,
     const void         *data,
     uint32_t            data_len
 );
-extern int kvarena_pop(KVArena *kva_p);
-extern int kvarena_grow(KVArena *kva_p);
-extern int kvarena_mark_dead(
+LIBKV_API int kvarena_pop(KVArena *kva_p);
+LIBKV_API int kvarena_grow(KVArena *kva_p);
+LIBKV_API int kvarena_mark_dead(
     KVArena    *kva_p,
     uint32_t    ent_idx
 );
-extern int kvarena_compact(KVArena **kva_pp);
-extern int32_t kvarena_get(
+LIBKV_API int kvarena_compact(KVArena **kva_pp);
+LIBKV_API int32_t kvarena_get(
     const KVArena      *kva_p,
     uint32_t            ent_idx,
     KVArenaEntryView   *out_entview_p
 );
 
-extern uint32_t kvarena_size(KVArena *kva_p);
-extern uint32_t kvarena_capacity(KVArena *kva_p);
-extern uint32_t kvarena_data_len(KVArena *kva_p);
-extern uint32_t kvarena_data_size(KVArena *kva_p);
-extern int kvarena_is_full(KVArena *kva_p);
-extern int kvarena_is_empty(KVArena *kva_p);
-extern void *kvarena_data(KVArena *kva_p);
-extern void *kvarena_entrytbl(KVArena *kva_p);
+LIBKV_API uint32_t kvarena_size(KVArena *kva_p);
+LIBKV_API uint32_t kvarena_capacity(KVArena *kva_p);
+LIBKV_API uint32_t kvarena_data_len(KVArena *kva_p);
+LIBKV_API uint32_t kvarena_data_size(KVArena *kva_p);
+LIBKV_API int kvarena_is_full(KVArena *kva_p);
+LIBKV_API int kvarena_is_empty(KVArena *kva_p);
+LIBKV_API void *kvarena_data(KVArena *kva_p);
+LIBKV_API void *kvarena_entrytbl(KVArena *kva_p);
 
-extern int32_t kvarena_push_auto_grow(
+LIBKV_API int32_t kvarena_push_auto_grow(
     KVArena            *kva_p,
     const kvht_key_t   *kvht_key_p,
     const void         *data,
     uint32_t            data_len
 );
 
-extern const KVArenaEntry *kvarena_get_entry(
+LIBKV_API const KVArenaEntry *kvarena_get_entry(
     const KVArena  *kva_p,
     uint32_t        ent_idx
 );
-extern const KVArenaEntryView *kvarena_entry_to_entview(
+LIBKV_API const KVArenaEntryView *kvarena_entry_to_entview(
     const KVArena      *kva_p,
     const KVArenaEntry *ent_p,
     KVArenaEntryView   *out_entview_p
